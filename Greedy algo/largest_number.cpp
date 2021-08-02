1164fdd79e5f760575ae41a2db9443564c10705d
Rajbir Banerjee <64858573+rajbir21-droid@users.noreply.github.com>
2021-08-02 17:05:04 +0530

    Update largest_number.cpp

--- Greedy algo/largest_number.cpp
+++ Greedy algo/largest_number.cpp
@@ -1,3 +1,10 @@
+/* 
+ Q) As the last question of a successful interview, your boss gives you a few pieces of paper
+with numbers on it and asks you to compose a largest number from these numbers. The
+resulting number is going to be your salary, so you are very much interested in maximizing
+this number. How can you do this?
+*/
+//Solution
 #include<bits/stdc++.h>
 using namespace std;
 struct{
