9024276c981e0c381e8352748333808af72577ae
Rajbir Banerjee <64858573+rajbir21-droid@users.noreply.github.com>
2021-08-08 20:32:36 +0530

    Update Count Triplets with sum smaller than X.cpp

--- Searching and Sorting/Count Triplets with sum smaller than X.cpp
+++ Searching and Sorting/Count Triplets with sum smaller than X.cpp
@@ -1,3 +1,35 @@
+/*
+Given an array arr[] of distinct integers of size N and a sum value X, the task is to find count of triplets with the sum smaller than the given sum value X.
+
+
+Example 1:
+
+Input: N = 4, X = 2
+arr[] = {-2, 0, 1, 3}
+Output:  2
+Explanation: Below are triplets with 
+sum less than 2 (-2, 0, 1) and (-2, 0, 3). 
+
+
+Example 2:
+
+Input: N = 5, X = 12
+arr[] = {5, 1, 3, 4, 7}
+Output: 4
+Explanation: Below are triplets with 
+sum less than 12 (1, 3, 4), (1, 3, 5), 
+(1, 3, 7) and (1, 4, 5).
+
+Expected Time Complexity: O(N2).
+Expected Auxiliary Space: O(1).
+
+
+Constraints:
+3 ≤ N ≤ 103
+*/
+
+
+
 // { Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
@@ -38,6 +70,7 @@ int main()
    
    	int t;
     cin >> t;
+   //enter no of test cases
     while (t--)
     {
     	int n;
@@ -60,3 +93,4 @@ int main()
     }
     return 0;
 }
+
